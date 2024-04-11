006e251aa1719bad2685888d189c164eedf1b195
Dekr0 <dekr0.dk@protonmail.com>
2024-04-11 01:03:07 -0600

    misc

--- lua/richpresence/utils.lua
+++ lua/richpresence/utils.lua
@@ -4,13 +4,13 @@ function M.cwd()
     return vim.fs.basename(vim.fn.getcwd())
 end
 
-function M.open_as_workspace()
+function M.open_as_workplace()
     return vim.fn.getcwd() == vim.api.nvim_buf_get_name(0)
 end
 
 function M.cf()
     local full_name = vim.api.nvim_buf_get_name(0)
-    if (string.len(full_name) == 0) then return end
+    if (string.len(full_name) == 0) then return "", "" end
     local raw = vim.fs.basename(full_name)
     local l, h = string.find(raw, "%.")
     while h do
@@ -25,6 +25,7 @@ function M.cf()
     else
         filename = raw
     end
+
     return filename, ext
 end
 
