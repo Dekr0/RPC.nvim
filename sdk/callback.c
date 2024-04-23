02bfd35d41d00468f8619a4c60076b739ceb0808
Dekr0 <dekr0.dk@protonmail.com>
2024-04-22 23:45:45 -0600

    remove unused callback

--- sdk/callback.c
+++ sdk/callback.c
@@ -1,10 +1,3 @@
 #include "callback.h"
-#include "sdk.h"
 
-void DISCORD_CALLBACK on_user_updated(void *data) {
-    Middleware *m = (Middleware *) data;
-
-    struct DiscordUser user; m->users->get_current_user(m->users, &user);
-
-    m->user_id = user.id;
-}
+void DISCORD_CALLBACK on_user_updated(void *data) {}
