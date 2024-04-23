5e89f77beda97c361dab505afe45909cf0727fe5
Dekr0 <dekr0.dk@protonmail.com>
2024-04-22 23:46:07 -0600

    nice formatting

--- sdk/sdk.c
+++ sdk/sdk.c
@@ -2,27 +2,20 @@
 #include <time.h>
 #include <unistd.h>
 #include "sdk.h"
-#include "init.h"
+#include "state.h"
 
 static int init (lua_State *L) {
     Middleware *m = (Middleware *) lua_newuserdata(L, sizeof(Middleware));
 
-    luaL_argcheck(L, m != NULL, 1, 
-            "failed to allocate memory for middleware inside Lua stack");
+    luaL_argcheck(L, m != NULL, 1, "failed to allocate memory for middleware inside Lua stack");
 
     const char *workspace = luaL_checkstring(L, 1);
     const char *filename  = luaL_checkstring(L, 2);
-    const char *ext = luaL_checkstring(L, 3);
-    const char *mode = luaL_checkstring(L, 4);
-    const unsigned apm = luaL_checknumber(L, 5);
+    const char *ext       = luaL_checkstring(L, 3);
+    const char *mode      = luaL_checkstring(L, 4);
+    const unsigned apm    = luaL_checknumber(L, 5);
 
-    conn_neovim(L, m);
     conn_discord(L, m);
-
-    luaL_argcheck(L, &(m->neovim_activity) != NULL, 1, 
-            "failed to allocate memory for struct DiscordActivty");
-
-    m->neovim_activity.timestamps.start = time(0);
 
     update_activity(m, workspace, filename, ext, mode, apm);
 
@@ -30,28 +23,22 @@ static int init (lua_State *L) {
 }
 
 static int run_callback(lua_State *L) {
-    Middleware *m = lua_touserdata(L, 1);
+    Middleware *m         = lua_touserdata(L, 1);
     const char *workplace = luaL_checkstring(L, 2);
     const char *filename  = luaL_checkstring(L, 3);
-    const char *ext = luaL_checkstring(L, 4);
-    const char *mode = luaL_checkstring(L, 5);
-    const unsigned apm = luaL_checknumber(L, 6);
+    const char *ext       = luaL_checkstring(L, 4);
+    const char *mode      = luaL_checkstring(L, 5);
+    const unsigned apm    = luaL_checknumber(L, 6);
 
     luaL_argcheck(L, m != NULL, 1, "passing nil");
 
     update_activity(m, workplace, filename, ext, mode, apm);
 
-    luaL_argcheck(L, m->core != NULL, 1,
-            "failed to run DiscordSDK callbacks. IDiscordcore is NULL");
+    luaL_argcheck(L, m->core != NULL, 1, "failed to run DiscordSDK callbacks. IDiscordcore is NULL");
 
-    luaL_argcheck(L, m->core->run_callbacks(m->core) == DiscordResult_Ok, 1,
-            "failed to run DiscordSDK callbacks");
+    luaL_argcheck(L, m->core->run_callbacks(m->core) == DiscordResult_Ok, 1, "failed to run DiscordSDK callbacks");
 
-    luaL_argcheck(L, !m->senderr, 1, "send error occur");
-
-    lua_pushinteger(L, m->user_id);
-
-    return 1;
+    return 0;
 }
 
 static int clean(lua_State *L) {
@@ -59,13 +46,9 @@ static int clean(lua_State *L) {
 
     luaL_argcheck(L, m != NULL, 1, "passing nil");
 
-    close(m->fd);
+    m->activities->clear_activity(m->activities, &m->activity_update_result, NULL);
 
-    m->activities->clear_activity(m->activities, &m->activity_update_result, 
-            NULL);
-
-    luaL_argcheck(L, m->core->run_callbacks(m->core) == DiscordResult_Ok, 1,
-            "failed to run DiscordSDK callbacks");
+    luaL_argcheck(L, m->core->run_callbacks(m->core) == DiscordResult_Ok, 1, "failed to run DiscordSDK callbacks");
 
     return 0;
 }
