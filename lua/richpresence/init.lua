36f4703e97e9e2f3316f7205dc6e3298062358f1
Dekr0 <dekr0.dk@protonmail.com>
2024-04-22 23:49:40 -0600

    added fields for user configuration, fields renaming to reflect their privacy

--- lua/richpresence/init.lua
+++ lua/richpresence/init.lua
@@ -1,24 +1,30 @@
-local Options = require("richpresence.options")
-local logger = require("richpresence.logger")
-local sdk    = require("richpresence.sdk")
+local logger   = require("richpresence.logger")
+local profiler = require("plenary.profile")
+local sdk      = require("richpresence.sdk")
 
 local namespace = "richpresence.nvim"
 
----@class M
----@field app App
-local M = { }
+---@class RichPresenceOptions
+---@field auto_update       boolean
+---@field auto_update_timer integer
+---@field logging           boolean
+---@field profiling         boolean
+local RichPresenceOptions = {}
+RichPresenceOptions.__index = RichPresenceOptions
 
 ---@class App
----@field discord    any 
----@field next_state State
----@field opts       Options
----@field timer      uv_timer_t
+---@field auto_update       boolean
+---@field auto_update_timer integer 
+---@field logging           boolean
+---@field profiling         boolean
+---@field __discord         any 
+---@field __next_state      State
+---@field __timer           uv_timer_t
 local App = {}
-
 App.__index = App
 
----@param opts Options
-function App:new(opts)
+---@param opts RichPresenceOptions
+function App:setup(opts)
     local next_state = require("richpresence.state")
     local discord = sdk.init(
         next_state.workplace,
@@ -29,27 +35,48 @@ function App:new(opts)
     )
     local timer = vim.uv.new_timer()
 
+    opts.auto_update_timer = opts.auto_update_timer or 10000
+    opts.auto_update_timer = opts.auto_update_timer > 5000 and
+        opts.auto_update_timer or 10000
+
     local app = setmetatable({
-        discord    = discord,
-        next_state = next_state,
-        opts       = Options(opts),
-        timer      = timer
+        auto_update       = opts.auto_update or false,
+        auto_update_timer = opts.auto_update_timer,
+        logging           = opts.logging or false,
+        profiling         = opts.profiling or false,
+        __discord         = discord,
+        __next_state      = next_state,
+        __timer           = timer
     }, self)
 
-    if opts.auto_update then
-        timer:start(5000, 5000, function () app:update() end)
+    if app.auto_update then
+        timer:start(
+            app.auto_update_timer,
+            app.auto_update_timer,
+            function () app:update() end
+        )
     end
+
+    vim.api.nvim_create_autocmd("BufEnter",   { callback = function ()
+        app.__next_state:on_buf_enter()
+    end})
+    vim.api.nvim_create_autocmd("DirChanged", { callback = function ()
+        -- get current directory after cd => vim.v.event["cwd"]
+    end})
+    vim.api.nvim_create_autocmd("VimLeave",   { callback = function ()
+        app:destroy()
+    end})
 
     return app
 end
 
 function App:destroy()
-    if self.timer then
-        self.timer:stop()
-        self.timer:close()
+    if self.__timer then
+        self.__timer:stop()
+        self.__timer:close()
     end
-    if self.discord then
-        sdk.clean(self.discord)
+    if self.__discord then
+        sdk.clean(self.__discord)
     end
 end
 
@@ -57,46 +84,36 @@ function App:log(f, m)
     logger:log(string.format("%s.%s: %s", namespace, f, m))
 end
 
+function App:show_log()
+    logger:show()
+end
+
 function App:update()
-    if self.opts.logging then
+    if self.profiling then profiler.start("profile.log", { flame = true }) end
+
+    if self.logging then
         self:log("run_callbacks", "run all pending DiscordSDK callbacks")
     end
 
     math.randomseed(os.time())
     math.random(); math.random(); math.random()
 
-    self.next_state.apm = math.random(60, 90)
+    self.__next_state.apm = math.random(60, 90)
 
-    if self.opts.logging then
-        self:log("run_sdk_callbacks", self.next_state:tostring())
+    if self.logging then
+        self:log("run_sdk_callbacks", self.__next_state:tostring())
     end
 
     sdk.run_callback(
-        self.discord,
-        self.next_state.workplace,
-        self.next_state.filename,
-        self.next_state.ext,
-        self.next_state.mode,
-        self.next_state.apm
+        self.__discord,
+        self.__next_state.workplace,
+        self.__next_state.filename,
+        self.__next_state.ext,
+        self.__next_state.mode,
+        self.__next_state.apm
     )
+
+    if self.profiling then profiler.stop() end
 end
 
----@param opts Options
-function M.setup(opts)
-    M.app = App:new(opts)
-    vim.api.nvim_create_autocmd("BufEnter",   { callback = function ()
-        M.app.next_state:on_buf_enter()
-    end})
-    vim.api.nvim_create_autocmd("DirChanged", { callback = function ()
-        -- get current directory after cd => vim.v.event["cwd"]
-    end})
-    vim.api.nvim_create_autocmd("VimLeave",   { callback = function ()
-        M.app:destroy()
-    end})
-end
-
-function M.show()
-    logger:show()
-end
-
-return M
+return App
