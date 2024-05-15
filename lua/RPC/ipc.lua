8e6e19135d97559e858922a91aa3fa2ef407a45b
Dekr0 <dekr0.dk@protonmail.com>
2024-05-14 22:04:31 -0600

    Handshake finish. Able to send a SET_ACTIVITY payload

--- lua/RPC/ipc.lua
+++ lua/RPC/ipc.lua
@@ -1,17 +1,60 @@
 local logger = require("RPC.logger")
 local pack = require("pack")
+local uuid = require("uuid")
 
 local n = "RPC.ipc"
 
 local client_id = "1222702873440157796"
 
+---@enum CMD
+local CMD = {
+    DISPATCH                       = "DISPATCH",
+    AUTHORIZE                      = "AUTHORIZE",
+    AUTHENTICATE                   = "AUTHENTICATE",
+    GET_GUILD                      = "GET_GUILD",
+    GET_GUILDS                     = "GET_GUILDS",
+    GET_CHANNEL                    = "GET_CHANNEL",
+    GET_CHANNELS                   = "GET_CHANNELS",
+    SUBSCRIBE                      = "SUBSCRIBE",
+    UNSUBSCRIBE                    = "UNSUBSCRIBE",
+    SET_USER_VOICE_SETTINGS        = "SET_USER_VOICE_SETTINGS",
+    SELECT_VOICE_CHANNEL           = "SELECT_VOICE_CHANNEL",
+    GET_SELECTED_VOICE_CHANNEL     = "GET_SELECTED_VOICE_CHANNEL",
+    SELECT_TEXT_CHANNEL            = "SELECT_TEXT_CHANNEL",
+    GET_VOICE_SETTINGS             = "GET_VOICE_SETTINGS",
+    SET_VOICE_SETTINGS             = "SET_VOICE_SETTINGS",
+    SET_CERTIFIED_DEVICES          = "SET_CERTIFIED_DEVICES",
+    SET_ACTIVITY                   = "SET_ACTIVITY",
+    SEND_ACTIVITY_JOIN_INVITE      = "SEND_ACTIVITY_JOIN_INVITE",
+    CLOSE_ACTIVITY_REQUEST         = "CLOSE_ACTIVITY_REQUEST"
+}
+
+---@enum Event
+local Event = {
+    READY                          = "READY",
+    ERROR                          = "ERROR",
+    GUILD_STATUS                   = "GUILD_STATUS",
+    GUILD_CREATE                   = "GUILD_CREATE",
+    CHANNEL_CREATE                 = "CHANNEL_CREATE",
+    VOICE_CHANNEL_SELECT           = "VOICE_CHANNEL_SELECT",
+    VOICE_STATE_CREATE             = "VOICE_STATE_CREATE",
+    VOICE_STATE_UPDATE             = "VOICE_STATE_UPDATE",
+    VOICE_STATE_DELETE             = "VOICE_STATE_DELETE",
+    VOICE_SETTINGS_UPDATE          = "VOICE_SETTINGS_UPDATE",
+    VOICE_CONNECTION_STATUS        = "VOICE_CONNECTION_STATUS",
+    SPEAKING_START                 = "SPEAKING_START",
+    SPEAKING_STOP                  = "SPEAKING_STOP",
+    MESSAGE_CREATE                 = "MESSAGE_CREATE",
+    MESSAGE_UPDATE                 = "MESSAGE_UPDATE",
+    MESSAGE_DELETE                 = "MESSAGE_DELETE",
+    NOTIFICATION_CREATE            = "NOTIFICATION_CREATE",
+    ACTIVITY_JOIN                  = "ACTIVITY_JOIN",
+    ACTIVITY_SPECTATE              = "ACTIVITY_SPECTATE",
+    ACTIVITY_JOIN_REQUEST          = "ACTIVITY_JOIN_REQUEST"
+}
+
 -- Types of binary message
----@class Opcode
----@field Handshake integer 
----@field Frame     integer 
----@field Close     integer 
----@field Ping      integer 
----@field Pong      integer 
+---@enum Opcode
 local Opcode = {
     Handshake = 0,
     Frame     = 1,
@@ -20,9 +63,20 @@ local Opcode = {
     Pong      = 4
 }
 
+---@class Payload
+---@field args      table   | nil
+---@field client_id string  | nil
+---@field cmd       string  | nil
+---@field data      table   | nil
+---@field evt       string  | nil
+---@field nonce     string  | nil
+---@field v         integer | nil
+local Payload = {}
+
 ---@class IPC
 ---@field connected        boolean
 ---@field handshake_finish boolean
+---@field is_authenticated boolean
 ---@field logger           Logger
 ---@field pipe             uv_pipe_t
 local IPC = {}
@@ -43,6 +97,7 @@ function IPC:new()
     local instance = setmetatable({
         connected        = false,
         handshake_finish = false,
+        is_authenticated = false,
         pipe             = vim.loop.new_pipe(false)
     }, self)
 
@@ -55,19 +110,80 @@ function IPC:new()
     return instance
 end
 
+-- Abstraction on message read 
+-- Binary message structure used to communicate Discord via pipe (Windows) or 
+-- unix domain socket (Unix).
+--
+-- The binary message structure is a frame represented via a single byte array.
+--
+-- | header | data in JSON format | 
+--
+-- The header's structure => | opcode | length of entire frame |
+--      Opcode and length of entire frame are little endian unsigned 
+--      integers (32 bits).
+--      Make sure to convert them based on architecture specific 
+--
 ---@param req_opcode integer 
 ---@param chunk  string | nil 
-function IPC:read(req_opcode, chunk)
-    assert(req_opcode >= Opcode.Handshake 
+function IPC:read(req_opcode, req_cmd, chunk)
+    assert(req_opcode >= Opcode.Handshake
         and req_opcode <= Opcode.Pong, req_opcode)
 
     if not chunk then return end
 
     local res_opcode_chunk = string.sub(chunk, 1, 4)
     local res_length_chunk = string.sub(chunk, 5, 8)
+    local data = string.sub(chunk, 9)
+
+    local res_opcode = pack.unpack_uint32(res_opcode_chunk, 1)
+    local res_length = pack.unpack_uint32(res_length_chunk, 1)
+
+    assert(res_opcode >= Opcode.Handshake
+        and res_opcode <= Opcode.Pong, res_opcode)
+
+    local metainfo = string.format(
+        "Response Length: %d; "
+        .."Response Opcode: %d; "
+        .."Response JSON Length (from Header): %d; "
+        .."Response JSON Length (from Lua String Length): %d",
+        #chunk, res_opcode, res_length, #data)
+
+    logger:l(n, "IPC:read", metainfo)
+    logger:l(n, "IPC:read", chunk)
+
+    assert(res_length == #data,
+        "JSON length miss match with the header provided length")
+
+    if (res_opcode == Opcode.Frame) then
+        vim.schedule(function ()
+            self:parse_frame(req_opcode, req_cmd, data)
+        end)
+    end
 end
 
--- Abstraction on message delivery
+---@param req_opcode integer
+---@param data       string
+function IPC:parse_frame(req_opcode, req_cmd, data)
+    ---@type Payload | nil
+    local json_data = vim.fn.json_decode(data)
+    assert(json_data, "Nil JSON data")
+
+    if (req_opcode == Opcode.Handshake) then
+        self.handshake_finish = true
+
+        assert(json_data.cmd == CMD.DISPATCH)
+        assert(json_data.evt == Event.READY,
+            "Failed to establish a Discord RPC handshake")
+        self:set_activity()
+    elseif req_opcode == Opcode.Frame then
+        if req_cmd == CMD.AUTHORIZE then
+            assert(json_data.cmd == CMD.AUTHORIZE)
+            logger:l("", "", json_data.data.code)
+        end
+    end
+end
+
+-- Abstraction on message delivery 
 -- Binary message structure used to communicate Discord via pipe (Windows) or 
 -- unix domain socket (Unix).
 --
@@ -86,9 +202,9 @@ end
 --     entire buffer to the stream.
 -- 
 ---@param req_opcode      integer 
----@param t           table 
+---@param t               Payload 
 function IPC:write(req_opcode, t)
-    assert(req_opcode >= Opcode.Handshake 
+    assert(req_opcode >= Opcode.Handshake
         and req_opcode <= Opcode.Pong, req_opcode)
 
     local pl = vim.fn.json_encode(t)
@@ -100,8 +216,7 @@ function IPC:write(req_opcode, t)
     self.pipe:read_start(function(err, chunk)
         assert(not err, err)
         logger:l(n, "IPC:write: pipe:write", "...Payload arrived");
-        logger:l(n, "IPC:write: pipe:write", vim.inspect(chunk));
-        self:read(req_opcode, chunk)
+        self:read(req_opcode, t.cmd, chunk)
     end)
 
     self.pipe:write(pl, function (reason)
@@ -110,9 +225,55 @@ function IPC:write(req_opcode, t)
     end)
 end
 
+--- There is no need to go through the authentication process (OAuth2) as 
+--- suggested in the Discord Developer Documentation. You can send a 
+--- "SET_ACTIVITY" payload directly to the local RPC server. I think the OAuth2
+--- is for application that use WebSocket to communicate with the local RPC 
+-- server
+function IPC:set_activity()
+    local t = {
+        args = {
+            activity = {
+                state      = "In RPC | APM: 69",
+                details    = "Editing ipc.lua",
+                assets     = {
+                    large_image = "lua",
+                    small_image = "neovim"
+                }
+            },
+            instance = true,
+            pid = vim.loop:os_getpid()
+        },
+        cmd = "SET_ACTIVITY",
+        nonce = uuid.uuid4()
+    }
+
+    self:write(Opcode.Frame, t)
+end
+
+function IPC:authenticate()
+    if self.is_authenticated then return end
+
+    ---@type Payload
+    local t = {
+        args = {
+            client_id = client_id,
+            scopes =  {
+                "rpc",
+                "identify"
+            },
+        },
+        cmd = "AUTHORIZE",
+        nonce = uuid.uuid4()
+    }
+
+    self:write(Opcode.Frame, t)
+end
+
 function IPC:handshake()
     if self.handshake_finish then return end
 
+    ---@type Payload
     local t = { client_id = client_id, v = 1 }
 
     self:write(Opcode.Handshake, t)
