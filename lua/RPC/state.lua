5b56cd6f067f107e849d18356caf1489adfee468
Dekr0 <dekr0.dk@protonmail.com>
2024-05-18 04:21:00 -0600

    function for translate field of a State instance into Activity object in the Set Activity Payload; callback function when directory change

--- lua/RPC/state.lua
+++ lua/RPC/state.lua
@@ -1,37 +1,121 @@
 local utils = require("RPC.utils")
+local logger = require("RPC.logger")
+
+---@class Timestamps
+---@field start integer
+---@field end   integer | nil
+
+---@class Assets
+---@field large_image string
+---@field large_text  string | nil
+---@field small_image string
+---@field small_text  string | nil
+
+---@class Activity
+---@field assets         Assets
+---@field details        string
+---@field instance       boolean
+---@field state          string
+---@field timestamps     Timestamps
+
+---@enum Icon 
+local Icon = {
+    C         = "c",
+    CSS       = "css",
+    GIT       = "git",
+    GO        = "go",
+    H         = "h",
+    JS        = "js",
+    LUA       = "lua",
+    MAKEFILE  = "makefile",
+    MD        = "md",
+    TS        = "ts",
+    INSERT    = "insert",
+    NORMAL    = "normal",
+    REPLACE   = "replace",
+    VISUAL    = "visual",
+}
 
 ---@class State
----@field workplace string
----@field filename string
----@field ext string
----@field mode string
----@field apm number
+---@field activity        Activity | nil
+---@field apm             number
+---@field custom_details  string 
+---@field custom_state    string
+---@field ext             string
+---@field filename        string
+---@field workplace       string
+---@field mode            string
 local State = {}
-
 State.__index = State
 
+local MAX_TEXT_LEN  = 128
+local MAX_EXT_LEN   = 12
+local STATE_PREFIX       = "In "
+local APM_PREFIX         = " | APM: "
+local DETAILS_PREFIX     = "Editing ."
+local FILENAME_POSTFIX   = "... "
+local POSTFIX            = "..."
+local MAX_WORKPLACE_LEN = MAX_TEXT_LEN - #STATE_PREFIX - #APM_PREFIX - 3
+
+--[[
+--  custom_state and custom_detail are strings respectively that can be either a
+--  pure plain text, or pure directives (indicate by square bracket []), or a
+--  mix of above two. The directives will be parsed and be replace to the 
+--  corresponding content.
+--
+--  Following are the directives that can be parsed (must be exact match)
+--      - [file] => replace with current active buffer name
+--      - [mode] => replace with current active mode
+--      - [workplace] => replace with current working directory
+--      - [apm] => replace with user's APM (Action Per Minute)
+--      directory
+-- 
+-- An example: 
+--      custom_detail = "Editing: [file] | Mode: [mode]",
+--      custom_state  ="In workplace: [workplace] | APM: [apm]"
+--]]
+---@param custom_details string | nil
+---@param custom_state   string | nil
 ---@return State
-function State:new()
+function State:new(custom_details, custom_state)
     local filename = ""
     local ext = ""
     if not utils.open_as_workplace() then
-        filename, ext = utils.cf()
+        filename, ext = utils.get_name_ext()
     end
+
+    ---@type Activity
+    ---@diagnostic disable
+    local activity = {
+        timestamps = {
+            start = os.time() -- Need replacement 
+        },
+        instance = true
+    }
+
+    ---@type State
     local state = setmetatable({
-        workplace = utils.cwd(),
-        filename = filename,
+        activity = activity,
+        apm = 0,
         ext = ext,
+        custom_detail = custom_details or "Editing: [file] | Mode: [mode]",
+        custom_state  = custom_state or "In workplace: [workplace] | APM: [apm]",
+        filename = filename,
         mode = "Normal",
-        apm = 0
+        workplace = utils.cwd(),
     }, self)
+
+    state:set_activity()
 
     return state
 end
 
 function State:tostring()
-    return string.format("filename = %s, ext = %s", self.filename, self.ext)
+    return string.format("filename = %s, ext = %s, workplace = %s", 
+        self.filename, self.ext, self.workplace)
 end
 
+-- True means there is state changed. False means there is no state changed.
 ---@return boolean
 function State:on_buf_enter()
     local buf_name = vim.api.nvim_buf_get_name(0)
@@ -41,9 +125,8 @@ function State:on_buf_enter()
     if (string.len(buf_name) == 0) then
         return false
     end
-    local filename, ext = utils.cf()
+    local filename, ext = utils.get_name_ext()
 
-    -- exact same file name but in a different directory
     if filename == self.filename and ext == self.ext then
         return false
     end
@@ -54,4 +137,55 @@ function State:on_buf_enter()
     return true
 end
 
-return State:new()
+---@param workplace string | nil
+function State:on_dir_changed(workplace)
+    self.workplace = workplace and vim.fs.basename(workplace) or "???"
+end
+
+-- Not support custom state and details yet.
+function State:set_activity()
+    math.randomseed(os.time())
+    math.random(); math.random(); math.random() -- warm up
+
+    self.apm = math.random(60, 90)
+
+    if self.filename == "" then
+        self.activity.assets = {
+            large_image = "neovim",
+            large_text  = string.format("In %s", self.workplace),
+            small_image = Icon.NORMAL,
+            small_text  = "In Normal mode"
+        }
+    else
+        self.activity.assets = {
+            large_image = Icon[self.ext:upper()] or "unknown",
+            large_text  = string.format("%s.%s", self.filename, self.ext),
+            small_image = Icon.INSERT,
+            small_text  = string.format("In %s mode", Icon.INSERT)
+        }
+    end
+
+    if self.filename == "" then self.activity.details = ""
+    else
+        local filename_lim = MAX_TEXT_LEN - #DETAILS_PREFIX - 
+            (#self.ext < MAX_EXT_LEN and #self.ext or MAX_EXT_LEN)
+
+        self.activity.details = string.format("Editing %s.%s",
+            #self.filename <= filename_lim and self.filename 
+                or self.filename:sub(1, filename_lim - #FILENAME_POSTFIX)..FILENAME_POSTFIX,
+
+            #self.ext <= MAX_EXT_LEN and self.ext 
+                or self.ext:sub(1, MAX_EXT_LEN - #POSTFIX)..POSTFIX
+        )
+    end
+    
+    self.activity.state = string.format("In %s | APM: %d",
+
+        #self.workplace < MAX_WORKPLACE_LEN and self.workplace or 
+            self.workplace:sub(1, MAX_WORKPLACE_LEN - #POSTFIX).."POSTFIX",
+
+        self.apm
+    )
+end
+
+return State
