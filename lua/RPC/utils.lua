258b19ab2879be540f8283ef85f7fba378f2474a
Dekr0 <dekr0.dk@protonmail.com>
2024-05-18 04:17:08 -0600

    document a function that extract filename and extension

--- lua/RPC/utils.lua
+++ lua/RPC/utils.lua
@@ -8,22 +8,34 @@ function M.open_as_workplace()
     return vim.fn.getcwd() == vim.api.nvim_buf_get_name(0)
 end
 
-function M.cf()
+-- Extract the name and extension of a file
+-- If a file does not extension, name will be the same as the original filename,
+--  and extension will be an empty string.
+---@return string
+---@return string
+function M.get_name_ext()
     local full_name = vim.api.nvim_buf_get_name(0)
     if (string.len(full_name) == 0) then return "", "" end
-    local raw = vim.fs.basename(full_name)
-    local l, h = string.find(raw, "%.")
-    while h do
-      h, _ = string.find(raw, "%.", l + 1)
-      if h then l = h else break end
+    local basename = vim.fs.basename(full_name)
+
+    -- Find the last matched of the "." character since it's common to see 
+    -- files to have multiple dots in their names
+    -- We need an extra temporary variable to keep tract the previous "." 
+    -- character if the next iteration find no more "." character and set our
+    -- index to nil.
+    local prev_idx, next_idx = string.find(basename, "%.")
+    while next_idx do
+      next_idx, _ = string.find(basename, "%.", prev_idx + 1)
+      if next_idx then prev_idx = next_idx else break end
     end
+
     local filename = ""
     local ext = ""
-    if l then
-        filename = string.sub(raw, 0, l - 1)
-        ext = string.sub(raw, l + 1)
+    if prev_idx then
+        filename = string.sub(basename, 0, prev_idx - 1)
+        ext = string.sub(basename, prev_idx + 1)
     else
-        filename = raw
+        filename = basename
     end
 
     return filename, ext
