85e324a7019c1419bdfd38efdf39334f31afbdb9
Dekr0 <dekr0.dk@protonmail.com>
2024-05-18 04:16:41 -0600

    move custom_detail and custom_state to State Class

--- lua/RPC/options.lua
+++ lua/RPC/options.lua
@@ -1,26 +1,7 @@
---[[
---  custom_state and custom_detail are strings respectively that can be either a
---  pure plain text, or pure directives (indicate by square bracket []), or a
---  mix of above two. The directives will be parsed and be replace to the 
---  corresponding content.
---
---  Following are the directives that can be parsed (must be exact match)
---      - [file] => replace with current active buffer name
---      - [mode] => replace with current active mode
---      - [workplace] => replace with current working directory
---      - [apm] => replace with user's APM (Action Per Minute)
---      directory
--- 
--- An example: 
---      custom_detail = "Editing: [file] | Mode: [mode]",
---      custom_state  ="In workplace: [workplace] | APM: [apm]"
---]]
 ---@class Opts
 ---@field apm               boolean
 ---@field auto_update       boolean
 ---@field auto_update_timer integer | nil
----@field custom_detail     string  | nil 
----@field custom_state      string  | nil
 ---@field logging           boolean 
 ---@field profiling         boolean
 local Opts = {}
@@ -36,8 +17,6 @@ function Opts:new(usr)
        apm               = usr.apm or false,
        auto_update       = usr.auto_update or false,
        auto_update_timer = usr.auto_update_timer or false,
-       custom_detail     = usr.custom_detail or "Editing: [file] | Mode: [mode]",
-       custom_state      = usr.custom_state or "In workplace: [workplace] | APM: [apm]",
        logging           = usr.logging or true,
        profiling         = usr.profiling or false
    }, self)
