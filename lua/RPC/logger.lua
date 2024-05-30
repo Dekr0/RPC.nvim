8ab6178469c16dac0867d9f71a7a0c07621d2612
Dekr0 <dekr0.dk@protonmail.com>
2024-05-29 23:15:05 -0600

    added logging level to filter out based on given logging level

--- lua/RPC/logger.lua
+++ lua/RPC/logger.lua
@@ -1,33 +1,48 @@
+---@enum LEVEL
+local LEVEL = {
+    TRACE = 10,
+    DEBUG = 20,
+    INFO  = 30,
+    WARN  = 40,
+    ERROR = 50,
+    FATAL = 60
+}
+
 ---@class Logger
----@field lines string[]
+---@field LEVEL     Enum
+---@field filter    LEVEL
+---@field lines     string[]
 ---@field max_lines number
 local Logger = {}
+
+local ns = "RPC.logger"
 
 ---@type Logger | nil
 local instance = nil
 
+Logger.LEVEL = LEVEL
+
 Logger.__index = Logger
 
-function Logger:l(n, f, m)
-    self:log(string.format("%s.%s: %s", n, f, m))
+
+function Logger:clear() self.lines = {} end
+
+--- A helper function for produce formatted log message.
+---@param l LEVEL  -- log level
+---@param n string -- name of the module that invokes logger
+---@param f string -- name of the function that invokes logger
+---@param m string -- log message
+function Logger:l(l, n, f, m)
+    if l < self.filter then return end
+
+    self:log(l, string.format("%s.%s: %s", n, f, m))
 end
 
----@return Logger
-function Logger:new()
-    if instance then
-        return instance
-    end
+---@param level LEVEL
+---@param line  string
+function Logger:log(level, line)
+    if level < self.filter then return end
 
-    instance = setmetatable({
-        lines = {},
-        max_lines = 50,
-    }, self)
-
-    return instance
-end
-
----@param line string
-function Logger:log(line)
     table.insert(self.lines, line)
 
     while #self.lines > self.max_lines do
@@ -35,8 +50,33 @@ function Logger:log(line)
     end
 end
 
-function Logger:clear()
-    self.lines = {}
+---@param filter LEVEL | nil
+---@return Logger
+function Logger:new(filter)
+    if instance then return instance end
+
+    instance = setmetatable({
+        filter = filter or LEVEL.DEBUG,
+        lines = {},
+        max_lines = 50,
+    }, self)
+
+    return instance
+end
+
+---@param filter any
+function Logger:set_filter(filter)
+    if type(filter) ~= "number" then
+        self:l(40, ns, "Logger:set_filter", "Invalid log level filter")
+        return
+    end
+
+    if filter < LEVEL.TRACE or filter > LEVEL.FATAL then
+        self:l(40, ns, "Logger:set_filter", "Invalid log level filter")
+        return
+    end
+
+    self.filter = filter
 end
 
 function Logger:show()
@@ -45,4 +85,4 @@ function Logger:show()
     vim.api.nvim_win_set_buf(0, bufnr)
 end
 
-return Logger:new()
+return Logger:new(20)
