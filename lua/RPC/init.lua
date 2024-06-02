b7e87740ba9e81221e995e403adf95bd7b54d8aa
Dekr0 <dekr0.dk@protonmail.com>
2024-06-02 07:35:37 -0600

    added function for users to close and reopen connection with Discord

--- lua/RPC/init.lua
+++ lua/RPC/init.lua
@@ -4,13 +4,15 @@ local Opts = require("RPC.options")
 
 local n = "RPC.init"
 
+
 ---@class App
 ---@field opts         Opts
----@field __IPC        IPC
+---@field __IPC        IPC | nil
 ---@field __state      State
----@field __timer      uv_timer_t
+---@field __timer      uv_timer_t | nil
 local RPC = {}
 RPC.__index = RPC
+
 
 function RPC:setup(usr)
     assert(usr.ipc_path, "Please specify the absolute path of discord IPC file.")
@@ -23,7 +25,7 @@ function RPC:setup(usr)
 
     local app = setmetatable({
         opts         = opts,
-        __IPC        = require("RPC.ipc"):new(usr.ipc_path),
+        __IPC        = require("RPC.ipc"):new(opts.ipc_path),
         __state      = require("RPC.state"):new(),
         __timer      = timer
     }, self)
@@ -53,12 +55,13 @@ function RPC:setup(usr)
     return app
 end
 
+
 function RPC:destroy()
     if self.__timer then
         self.__timer:stop()
         self.__timer:close()
     end
-    self.__IPC:destroy()
+    if self.__IPC then self.__IPC:destroy() end
 end
 
 function RPC:update()
@@ -71,24 +74,84 @@ function RPC:update()
     logger:l(20, n, "RPC:update", self.__state:tostring())
 end
 
+
 function RPC:show_log() logger:show() end
 
-function RPC:pause_update()
+
+--- Pause the update loop for Discord Rich Presence 
+---@param clear boolean Clear activity status if it is true
+function RPC:pause_update(clear)
     if self.__timer then self.__timer:stop() end
+
+    if clear then
+        vim.schedule(function ()
+            logger:l(30, n, "RPC:update", string.format("Clearing activity"))
+
+            self.__state:clear_activity()
+
+            self.__IPC:set_activity(self.__state.activity)
+        end)
+    end
 end
 
+
+--- Resume the update loop for Discord Rich Presence
 function RPC:resume_update()
+    assert(self.__IPC, "Connection with Discord is permantely closed. Please reset the \
+        plugin state with RPC:close() to clear out potential dirty state. Then \
+        reopen a new connection with Discord and a new update loop with \
+        RPC:open()")
+    assert(self.__timer, "Update loop is permantely closed. Please reset the \
+        plugin state with RPC:close() to clear out potential dirty state. Then \
+        reopen a new connection with Discord and a new update loop with \
+        RPC:open()")
+    self.__timer:start(
+        self.opts.auto_update_timer,
+        self.opts.auto_update_timer,
+        function ()
+           if self.__IPC.handshake_finish then
+                vim.schedule(function () self:update() end)
+           end
+        end
+    )
+end
+
+
+--- Close the connection with Discord
+function RPC:close()
     if self.__timer then
+        self.__timer:stop()
+        self.__timer = nil
+    end
+    if self.__IPC then
+        self.__IPC:destroy()
+        self.__IPC = nil
+    end
+end
+
+
+function RPC:open()
+    assert(not self.__IPC and not self.__timer, "Failed to open a new a new \
+        connection with Discord, and then start a new update loop. Reason: \
+        there's an existence connection is still open, or there's an existence \
+        update loop that is still running. Please use RPC:close() to clear out \
+        the entire state first before calling RPC:open()")
+
+    self.__IPC = require("RPC.ipc"):new(self.opts.ipc_path)
+    self.__timer = vim.uv.new_timer()
+
+    if self.opts.auto_update then
         self.__timer:start(
             self.opts.auto_update_timer,
             self.opts.auto_update_timer,
             function ()
-               if self.__IPC.handshake_finish then
+                if self.__IPC.handshake_finish then
                     vim.schedule(function () self:update() end)
-               end
+                end
             end
         )
     end
 end
+
 
 return RPC
